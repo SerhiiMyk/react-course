05cbbf5d39324f220671d20c632a34eac5466862
SerhiiMyk <sergey.mykolaichuk@gmail.com>
2021-08-24 21:24:15 +0300

    done

--- src/services/car.service.js
+++ src/services/car.service.js
@@ -1,5 +1,5 @@
 export const saveCar= ({model,price,year}) => {
-    return fetch('http://195.72.146.25/api/v1/cars', {
+    return fetch('http://91.201.233.14/api/v1/cars', {
         method: 'POST',
         body: JSON.stringify({model,price,year}),
         headers: {
