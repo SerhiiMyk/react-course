e1b594636d6e362c3563012538f3ab408ae0028c
SerhiiMyk <sergey.mykolaichuk@gmail.com>
2021-09-08 02:22:19 +0300

    done

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 import {loadGenres, loadMovies} from "../redux/action";
 
-const apiKey = '24d2953612c72c45a876d2a49471a079'
+const apiKey = '24d2953612c72c45a876d2a49471a079';
 const instance = axios.create({
-
     baseURL: 'https://api.themoviedb.org/3',
     headers: {
         Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNGQyOTUzNjEyYzcyYzQ1YTg3NmQyYTQ5NDcxYTA3OSIsInN1YiI6IjYxMmZkYWE0NmEzNDQ4MDA5MGY1NjA5MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M4XsfMMbY-Q8udTjAOvFWRYVJy9j2yFMsBeh_e05fgk'
@@ -12,13 +11,13 @@ const instance = axios.create({
 
 const getMovie = (page, genreId) => async (dispatch) => {
     let response = await instance.get(`/discover/movie?api_key=${apiKey}&page=${page}&with_genres=${genreId}`);
-    dispatch(loadMovies(response.data))
+    dispatch(loadMovies(response.data));
 }
 
 
 const getGenre = () => async (dispatch) => {
     let response = await instance.get('/genre/movie/list');
-    dispatch(loadGenres(response.data.genres))
+    dispatch(loadGenres(response.data.genres));
 }
 
 export {getMovie, getGenre}
